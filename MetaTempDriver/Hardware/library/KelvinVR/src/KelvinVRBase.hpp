b0169e9c359a0b5f90ebe53ff51ac1eea14723f5
Alex Noyanov <alex.noyanov@gmail.com>
2024-09-22 12:15:33 +0300

    KelvinVR: Thermal Cycle example sketch + new methods

--- MetaTempDriver/Hardware/library/KelvinVR/src/KelvinVRBase.hpp
+++ MetaTempDriver/Hardware/library/KelvinVR/src/KelvinVRBase.hpp
@@ -4,6 +4,7 @@
 class KelvinVRBase
 {
 public:
+    virtual void runThermalCycle(bool isCooling, int intensity, int durationSeconds) = 0;
     virtual void begin() = 0;
     virtual void update() = 0;
     // Add other common functions here
--- MetaTempDriver/Hardware/library/KelvinVR/src/hardware/esp32/KelvinVRESP32.hpp
+++ MetaTempDriver/Hardware/library/KelvinVR/src/hardware/esp32/KelvinVRESP32.hpp
@@ -6,6 +6,7 @@
 class KelvinVRESP32 : public KelvinVRBase
 {
 public:
+    void runThermalCycle(bool isCooling, int intensity, int durationSeconds) override;
     void begin() override;
     void update() override;
     // Implement other functions
